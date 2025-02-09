844affbc3a839d2e6f5db89330e29dfb09bf4e90
Miguel Filipe <miguel.filipe@gmail.com>
2025-02-09 01:12:05 +0000

    now with claude's fiber scheduler, can't notice difference.

--- cpp/src/server.cpp
+++ cpp/src/server.cpp
@@ -1,7 +1,14 @@
-#include "pingpong.grpc.pb.h"
 #include <boost/fiber/all.hpp>
 #include <chrono>
 #include <grpcpp/grpcpp.h>
+#include <mutex>
+#include <thread>
+
+#include "pingpong.grpc.pb.h"
+
+using std::condition_variable;
+using std::mutex;
+using std::unique_lock;
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -10,13 +17,61 @@ using grpc::ServerReaderWriter;
 using grpc::Status;
 using namespace pingpong;
 
+// Custom fiber scheduler optimized for RPC
+class RPCScheduler : public boost::fibers::algo::algorithm {
+
+  boost::fibers::scheduler::ready_queue_type rqueue_{};
+  mutable std::mutex mtx_{};
+  std::condition_variable cv_{};
+  bool flag_{false};
+
+public:
+  void awakened(boost::fibers::context *ctx) noexcept override {
+    std::unique_lock<std::mutex> lk(mtx_);
+    rqueue_.push_back(*ctx);
+  }
+
+  boost::fibers::context *pick_next() noexcept override {
+    std::unique_lock<std::mutex> lk(mtx_);
+    boost::fibers::context *ctx(nullptr);
+    if (!rqueue_.empty()) {
+      ctx = &rqueue_.front();
+      rqueue_.pop_front();
+    }
+    return ctx;
+  }
+
+  bool has_ready_fibers() const noexcept override {
+    std::unique_lock<std::mutex> lk(mtx_);
+    return !rqueue_.empty();
+  }
+
+  void suspend_until(std::chrono::steady_clock::time_point const
+                         &time_point) noexcept override {
+    std::unique_lock<std::mutex> lk(mtx_);
+    if (!flag_) {
+      cv_.wait_until(lk, time_point);
+    }
+  }
+
+  void notify() noexcept override {
+    std::unique_lock<std::mutex> lk(mtx_);
+    flag_ = true;
+    cv_.notify_all();
+  }
+};
+
 class BasicPingPongService final : public PingPong::Service {
   Status StreamPingPong(ServerContext *context,
                         ServerReaderWriter<Pong, Ping> *stream) override {
+    boost::fibers::use_scheduling_algorithm<RPCScheduler>();
+
     // Simple fiber to handle each connection
-    boost::fibers::fiber([stream]() {
+    // boost::fibers::fiber([stream]()
+    {
       Ping ping;
       while (stream->Read(&ping)) {
+        // boost::this_fiber::sleep_for(std::chrono::microseconds(10));
         Pong pong;
         pong.set_sequence(ping.sequence());
         pong.set_timestamp(ping.timestamp());
@@ -24,12 +79,14 @@ class BasicPingPongService final : public PingPong::Service {
             std::chrono::duration_cast<std::chrono::nanoseconds>(
                 std::chrono::high_resolution_clock::now().time_since_epoch())
                 .count());
+        pong.set_payload(ping.payload());
 
         if (!stream->Write(pong)) {
           break;
         }
       }
-    }).join();
+    }
+    //).join();
 
     return Status::OK;
   }
@@ -50,7 +107,7 @@ int main() {
 
   // Resource quota for better control
   auto resource_quota = grpc::ResourceQuota("pingpong_quota");
-  resource_quota.SetMaxThreads(2);  // Main + worker thread
+  resource_quota.SetMaxThreads(2); // Main + worker thread
   builder.SetResourceQuota(resource_quota);
 
   builder.RegisterService(&service);
